b05a946660cd0eb9c69e85a8fe550a7517bb6dcb
christybad <christy.badila@laetottenham.ac.uk>
2024-10-09 21:20:47 +0100

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -2,25 +2,9 @@
 
 # Define your reusable functions here:
 import math
-
-def calc(pOpp, pAdj):
-    # work out the hyp
-    pHyp = math.sqrt(pOpp**2 + pAdj**2)
-    return pHyp
-
-def validate_input(value):
-    while True:
-        if value <= 0:
-            value = float(input('enter a valid number: '))
-        else:
-            break
-    return value
-    
-
+from functions import *
 
 # Make sure each function only does ONE thing!!!!!!!!!!!
-
-
 
 ###########################################
 
