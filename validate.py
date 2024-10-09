28ff8f4495f445f5be579cc7ae11111f596e971e
christybad <christy.badila@laetottenham.ac.uk>
2024-10-09 21:16:22 +0100

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,1 +1,8 @@
-
+import math
+def validate_input(value):
+    while True:
+        if value <= 0:
+            value = float(input('enter a valid number: '))
+        else:
+            break
+    return value
